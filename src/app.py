70d269feb2ca7e96f7b5ef800044321ebd308a8f
nickStanisic <nicholasstanisic@gmail.com>
2024-06-10 17:07:26 -0600

    message

--- src/app.py
+++ src/app.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3
 
 from flask import Flask, render_template, request
-from weather import get_temperatures_for_colorado
+from weather import calculatePoints
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
 def index():
+    data = None
     if request.method == 'POST':
-        print("hi")
-    return render_template('index.html')
+        lowTemp = 0
+        highTemp = 0
+        startTime = 0
+        endTime = 0
+        startDate = 0
+        endDate = 0
+        lowTemp = request.form['lowTemp']
+        highTemp = request.form['highTemp']
+        startTime = request.form['startTime']
+        endTime = request.form['endTime']
+        startDate = request.form['startDate']
+        endDate = request.form['endDate']
+        data = calculatePoints(lowTemp, highTemp, startTime, endTime, startDate, endDate)
+    return render_template('index.html', data=data)
 
     # '''
     # <h1>Howdy</h1>
@@ -18,8 +31,3 @@ def index():
     #      <input type="submit" value="Submit!">
     #  </form>
     #  '''
-
-@app.route("/echo_user_input", methods=["POST"])
-def echo_input():
-    input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
--- src/weather.py
+++ src/weather.py
@@ -1,50 +1,40 @@
 import requests
 from dotenv import load_dotenv
 import os
-from dataclasses import dataclass
 
 load_dotenv()
 api_key = os.getenv('API_KEY')
-min_lat = 37
-lat_increases = 5
+min_lat = 41
+lat_increases = 3
 min_long = -108
-long_increases = 6
-
-@dataclass
-class TemperatureData:
-    time: int
-    temp: float
-
-@dataclass
-class coloradoData:
-    lat: int
-    long: int
-    temperature_data: list[TemperatureData]
-
+long_increases = 3
 
 def get_weather_data(lat, lon, API_key, units):
     resp = requests.get(f'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API_key}&units={units}').json()
     return resp
 
-def get_temperature_data(lat,lon):
-    data = get_weather_data(lat,lon,api_key,"imperial")
-    temperature_list = []
-    for i in range (0,data.get('cnt')):
-        tempData = TemperatureData(
-            time = data.get('list')[i].get('dt'),
-            temp = data.get('list')[i].get('main').get('temp')
-        )
-        temperature_list.append(tempData)
-    return temperature_list
-def get_temperatures_for_colorado():
+def get_temperatures_for_colorado(start, end, lowTemp, highTemp):
     colorado_temperature_list = []
-    for i in range (min_lat,min_lat + lat_increases):
+    for i in range (min_lat,min_lat - lat_increases, -1):
+        temps_for_lat = []
         for j in range (min_long,min_long + long_increases):
-            colorado_temperature = coloradoData (
-                lat = i,
-                long = j,
-                temperature_data = get_temperature_data(i,j)
-            )
-            colorado_temperature_list.append(colorado_temperature)
+            data = get_weather_data(i,j,api_key,"imperial")
+            withinRange = True
+            for k in range(start, end):
+                currentTemp = data.get('list')[i].get('main').get('temp')
+                if currentTemp < lowTemp or currentTemp > highTemp:
+                    withinRange = False
+            temps_for_lat.append(withinRange)
+        colorado_temperature_list.append(temps_for_lat)
     return colorado_temperature_list
+
+def calculatePoints(lowTemp, highTemp, startTime, endTime, startDate, endDate):
+    #range determined by date/time
+    print(lowTemp, highTemp, startTime, startDate, endTime, endDate)
+    start = int(startDate) + int(startTime)
+    end = int(endTime) + int(endDate) + 1
+    returnData = get_temperatures_for_colorado(start,end, lowTemp, highTemp)
+    print(returnData)
+    return returnData
+
 
